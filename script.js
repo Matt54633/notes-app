2b40859f7c3c6599dca06061ec1a4361f67f62fe
matt54633 <matt54633@gmail.com>
2024-04-27 18:54:43 +0100

    Amend Script

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
-const puppeteer = require('puppeteer');
+const puppeteer = require("puppeteer");
 
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto('https://notes.matt54633.com/');
-  await page.waitForSelector('#login-button'); 
-  await page.click('#login-button');
+  await page.goto("https://notes.matt54633.com/");
+  await page.waitForSelector("#login-button");
+  await page.click("#login-button");
+
+  const dialogPromise = new Promise(resolve => page.once('dialog', dialog => {
+    console.log('Dialog appeared with message:', dialog.message());
+    resolve();
+  }));
+
+  await dialogPromise;
   await browser.close();
 })();
