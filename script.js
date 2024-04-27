60eabe3f5b4196811fe2f6585657a8248f4b7ca0
matt54633 <matt54633@gmail.com>
2024-04-27 18:25:58 +0100

    Add Keep Alive

---
+++ script.js
@@ -0,0 +1,9 @@
+const puppeteer = require('puppeteer');
+
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto('https://notes.matt54633.com/');
+  await page.click('#login-button');
+  await browser.close();
+})();
