a7ac09fe042e6402ce9e994cf42e0cc14e55c45f
matt54633 <matt54633@gmail.com>
2024-04-27 18:30:53 +0100

    Fix action

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const puppeteer = require('puppeteer');
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.goto('https://notes.matt54633.com/');
+  await page.waitForSelector('#login-button'); 
   await page.click('#login-button');
   await browser.close();
 })();
