3a09a5b96e37001b4004fd9ce5e18dfd67522fc0
matt54633 <matt54633@gmail.com>
2024-05-05 10:37:50 +0100

    Add env

--- script.js
+++ script.js
@@ -1,17 +1,25 @@
 const puppeteer = require("puppeteer");
+require('dotenv').config();
 
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.goto("https://notes.matt54633.com/");
+  
+  await page.waitForSelector("#email-input");
+  await page.type("#email-input", process.env.USERNAME);
+
+  await page.waitForSelector("#password-input");
+  await page.type("#password-input", process.env.PASSWORD);
+
   await page.waitForSelector("#login-button");
   await page.click("#login-button");
 
-  const dialogPromise = new Promise(resolve => page.once('dialog', dialog => {
-    console.log('Dialog appeared with message:', dialog.message());
-    resolve();
-  }));
+  await page.waitForSelector("#settings-icon");
+  await page.click("#settings-icon");
 
-  await dialogPromise;
+  await page.waitForSelector("#logout-button");
+  await page.click("#logout-button");
+
   await browser.close();
 })();
